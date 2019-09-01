f750b98fcb5f054c94ec08114d7991480a1bad50
Amir <amircup2006@gmail.com>
2019-09-01 11:09:38 +0430

    Configuring the database

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=adcash',
     'username' => 'root',
-    'password' => '',
+    'password' => 'amir',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
